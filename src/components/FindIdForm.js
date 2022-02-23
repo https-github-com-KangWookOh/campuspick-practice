b2eda506c4cc9a0cf72c63954e0bd093f5330bea
YoonSeoRoh <yoonseo1997@naver.com>
2022-02-24 02:55:58 +0900

    edit

--- src/components/FindIdForm.js
+++ src/components/FindIdForm.js
@@ -1,6 +1,9 @@
 //아이디찾기 폼
 import React from "react";
 import styled from "styled-components";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Block = styled.div`
   position: relative;
@@ -44,12 +47,39 @@ const FindIdBtn = styled.button`
 `;
 
 function FindIdForm() {
+  const navigate = useNavigate();
+  const handleInput = (e) => {
+    e.preventDefault();
+    const obj = { pwd: e.target.value };
+    axios("", {
+      method: "POST",
+      data: obj,
+      headers: new Headers(),
+    })
+      .then(function (response) {
+        //비밀번호에 해당하는 아이디 존재함
+        console.log(response, "아이디 존재함");
+        alert(`아이디는 ${response.data}입니다.`);
+        navigate.push("/signup");
+      })
+      .catch(function (e) {
+        console.log(e);
+        alert(
+          "비밀번호에 해당하는 아이디가 존재하지 않습니다. 다시 입력해주세요."
+        );
+        window.location.replace("/FindPage");
+      });
+  };
   return (
     <Block>
-      <InsertForm>
+      <InsertForm onSubmit={handleInput}>
         <Span>아이디 찾기</Span>
-        <PwdInput placeholder="*비밀번호를 입력해주세요" />
-        <FindIdBtn>아이디 찾기</FindIdBtn>
+        <PwdInput
+          required
+          type="password"
+          placeholder="*비밀번호를 입력해주세요"
+        />
+        <FindIdBtn type="submit">아이디 찾기</FindIdBtn>
       </InsertForm>
     </Block>
   );
